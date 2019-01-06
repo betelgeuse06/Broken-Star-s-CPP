d6ab69b9d9fac0bf9b61c810368087257523395a
betelgeuse06 <32902105+betelgeuse06@users.noreply.github.com>
2019-01-06 22:53:07 +0900

    1/6

---
+++ C++ ( 12월 )/GFunctionOverloading.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  int floor101,floor101,floor101,floor101;
+  int floor201,floor202,floor203,floor203;
+  int floor301,floor302,floor303,floor304;
+  ....
+  return 0;
+}
